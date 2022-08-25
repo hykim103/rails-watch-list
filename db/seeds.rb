6b4c47168c2acb78dd1f2c2fdd4a91f0376eebbd
HaYeon Kim <kimhy8376@gmail.com>
2022-08-25 11:55:02 +0100

    Creaete Movie database fetching JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require "json"
+
+Movie.destroy_all
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+json = JSON.parse(URI.open(url).read)
+
+json["results"].each do |result|
+  Movie.create(
+    title: result["title"],
+    overview: result["overview"],
+    poster_url: result["poster_path"],
+    rating: result["vote_average"]
+  )
+end
