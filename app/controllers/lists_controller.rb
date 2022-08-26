bc91311df8d7375698563d802b89d8676d2f741a
HaYeon Kim <kimhy8376@gmail.com>
2022-08-26 16:52:17 +0100

    Enable to upload an imgae for a list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,10 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
   end
 
   def new
@@ -20,9 +20,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to root_path, status: :see_other
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name, :photo)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "lists#index"
-  resources :lists, only: %i[show new create] do
+  resources :lists, only: %i[show new create destroy] do
     resources :bookmarks, only: %i[create]
   end
   resources :bookmarks, only: :destroy
